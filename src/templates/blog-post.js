0fee3121203288b0d6e7f94d36e30edfb3c991bc
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-03-06 12:38:28 -0800

    Use exposed author and tags

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,37 +1,36 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import get from 'lodash/get';
-import EffectiveDiscussionsCommentsIframe from 'gatsby-plugin-ed-comments'
+import EffectiveDiscussionsCommentsIframe from 'gatsby-plugin-ed-comments';
 
 // import Bio from '../components/Bio';
 import { rhythm, scale } from '../utils/typography';
 
-<script src="https://embed.runkit.com" data-element-id="my-element"></script>
+<script src="https://embed.runkit.com" data-element-id="my-element" />;
+
+const post_style = {
+  ...scale(-1 / 5),
+  display: 'block',
+  marginBottom: rhythm(1),
+  marginTop: rhythm(-1),
+};
+
+const with_bottom_margin = { marginBottom: rhythm(1) };
 
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark;
+    const { title, tags, author, date } = post.frontmatter;
     const siteTitle = get(this.props, 'data.site.siteMetadata.title');
-
     return (
       <div>
-         <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} /> 
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}>
-          {post.frontmatter.date}
+        <Helmet title={`${title} | ${siteTitle}`} />
+        <h1>{title}</h1>
+        <p style={post_style}>
+          {date} | By {author} | {tags}
         </p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
+        <hr style={with_bottom_margin} />
         <EffectiveDiscussionsCommentsIframe discussionId={post.frontmatter.discussionId} />
       </div>
     );
@@ -53,6 +52,8 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
+        tags
+        author
         date(formatString: "MMMM DD, YYYY")
         discussionId
       }
