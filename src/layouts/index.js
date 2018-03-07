a318adb7df1c03c82a2adc555cf391995059acba
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-03-07 01:37:03 -0800

    whitespace simplify

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,16 @@ import { Container } from 'react-responsive-grid';
 
 import { rhythm, scale } from '../utils/typography';
 
+const link_style = { boxShadow: 'none', textDecoration: 'none', color: 'inherit' };
+
+const header_style = {
+  fontFamily: 'Montserrat, sans-serif',
+  marginTop: 0,
+  marginBottom: rhythm(-1),
+};
+
+const container_style = { maxWidth: rhythm(35), padding: `${rhythm(1.5)} ${rhythm(3 / 4)}` };
+
 class Template extends React.Component {
   render() {
     const { location, children } = this.props;
@@ -22,43 +32,22 @@ class Template extends React.Component {
             marginBottom: rhythm(1.5),
             marginTop: 0,
           }}>
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}>
+          <Link style={link_style} to={'/'}>
             yerevancoder
           </Link>
         </h1>
       );
     } else {
       header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}>
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}>
+        <h3 style={header_style}>
+          <Link style={link_style} to={'/'}>
             yerevancoder
           </Link>
         </h3>
       );
     }
     return (
-      <Container
-        style={{
-          maxWidth: rhythm(35),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}>
+      <Container style={container_style}>
         {header}
         {children()}
       </Container>
