8b82769a52616421b589572e2e5ef89992040249
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-03-09 08:53:16 -0800

    Remove signup dedicated route

--- src/pages/signup.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SignUpForm from '../components/signup';
-
-export default () => (
-  <div>
-    <h2>Sign up for account features</h2>
-    <SignUpForm />
-  </div>
-);
