93dd1f7a5e5416523acd3bde4a72edd9d5a2e865
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-03-09 13:59:11 -0800

    Simplify login

--- src/components/login.js
+++ src/components/login.js
@@ -41,11 +41,10 @@ export default withRouter(
       event.preventDefault();
       auth
         .signInWithEmailAndPassword(email, password)
-        .then(({ uid, refreshToken, metadata, email: email_account }) =>
-          userDidAuthSuccessfully({ uid, refreshToken, metadata, email_account })
-        )
-        .then(() => this.setState(() => ({ ...INITIAL_STATE })))
-        .then(user_did_sign_in)
+        .then(({ uid, refreshToken, metadata, email: email_account }) => {
+          userDidAuthSuccessfully({ uid, refreshToken, metadata, email_account });
+          user_did_sign_in();
+        })
         .catch(error => this.setState(updateByPropertyName('error', error)));
     };
 
