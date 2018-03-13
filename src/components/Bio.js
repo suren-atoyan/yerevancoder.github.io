dd4ddaf54f99cdb0bc1fba241763551f0c669ca2
Edgar Aroutiounian <edgar.factorial@gmail.com>
2018-03-13 15:31:56 -0700

    Give link on how to contribute

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -23,8 +23,9 @@ export default ({ style = {}, authors_count }) => (
     <span style={spacing_style}>
       yerevancoder.com is a place for coders in Armenia to share their thoughts, experiences in
       programming and tech. {authors_count} coders have already contributed and we are always
-      looking for more contributors, add a post{' '}
-      <a href={'https://github.com/yerevancoder/yerevancoder.github.io'}>here</a>
+      looking for more contributors, use this{' '}
+      <a href={'https://yerevancoder.com/2018-03-13-how-yerevan-coder-works/'}>post</a> as a guide
+      on how to add a new blog post.
     </span>
     <span>
       Post a tech job on our <Link to={ROUTES.JOBS_TABLE}>hiring board</Link>
